b32b1cae89a458a3db883bdbc90a6e0ee69b756f
lixiang <1193713569@qq.com>
2016-11-10 11:12:48 +0800

    save

--- superCalculator/matrix.h
+++ superCalculator/matrix.h
@@ -1,46 +1,48 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 #include "common.h"
 #include "matrixType.h"
-#include <vector>
 
 class Mat{
 public:
-
+	//construction function
 	Mat(void);
-	Mat(_INT rows, _INT cols);//随机数填充的矩阵
-	Mat(_INT rows, _INT cols,_INT value);//value填充的矩阵
-	Mat(string str);
+	Mat(_INT rows, _INT cols);//fill with random
+	Mat(_INT rows, _INT cols,_INT value);//fill with value
+	Mat(string str); //initialization with a string
+	Mat(Mat &mat_right);//copy construction
+	//destructor
+	~Mat();
 
 	//SG
 	_INT getRows();
 	_INT getCols();
-	_VOID setRows(_INT rows);
-	_VOID setCols(_INT cols);
-	//随机数填充
-	_VOID fill();
-	_VOID fill(_INT value);
-	//矩阵输出
-	friend ostream &operator <<(ostream &out,const Mat &mat_out);
-	//字符串解析
-	Mat strParse(string &str);
-	//矩阵加法
-	Mat operator +(const Mat &mat_right);
-	//矩阵减法
-	Mat operator -(const Mat &mat_right);
-	//深拷贝
-	_VOID deepcopy(const Mat &mat_original);
-	//拷贝构造
-	Mat(Mat &mat_right);
-	//赋值
-	Mat &operator=(Mat &mat_right);
-	//乘法
-	Mat operator*(const Mat &mat_right);
 
-	~Mat();
+	//fill
+	Mat &fill();  //random
+	Mat &fill(_INT value); //value
+	//deep copy
+	_VOID deepcopy(const Mat &mat_original);
+	//aassignment
+	Mat &operator=(Mat &mat_right);
+	//output matrix
+	friend ostream &operator <<(ostream &out,const Mat &mat_out);
+	//string parse
+	Mat strParse(string &str);
+	//addition
+	Mat operator +(const Mat &mat_right);
+	//subtraction
+	Mat operator -(const Mat &mat_right);
+	//multiplication
+	Mat operator*(const Mat &mat_right);
+	//transposition
+	Mat transposition();
+
 
 private:
-	_INT rows;
-	_INT cols;
-	_INT **element;
-
+	 _INT rows;
+	 _INT cols;
+	 _INT **element;
 };
